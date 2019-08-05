98b53357a26bacccb0175757b1640f2ce5145306
mlizchap <mlizchap@gmail.com>
2019-08-05 11:00:35 -0400

    added db connection

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
+const Sequelize = require('sequelize');
 
 app.get('/', (req, res) => res.send('hello'))
 
-const PORT = process.env.PORT || '8080'
+// database
+const dbConnect = 'postgres://localhost:5432/reddit';
+const db = new Sequelize(dbConnect, { logging: false });
 
+const eraseDatabaseOnSync = true;
+db.sync({ force: eraseDatabaseOnSync }).then(async => {
+    // if (eraseDatabaseOnSync) { seedDB(); }
+})
+
+
+const PORT = process.env.PORT || '8080'
 app.listen(PORT, () => console.log(`running on port ${PORT}`))
 
